472faefc72680e579ba4d9a7b2ddea6cfe1edb07
Stefan Berkner <stefan.berkner@tilotech.io>
2021-12-01 20:32:52 +0100

    Implement cognito module for M2M authorization and authentication

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,23 @@
+output "issuer_url" {
+  value       = format("https://%s", aws_cognito_user_pool.pool.endpoint)
+  description = "The issuer URL of the created Cognito User Pool for use with the API Gateway."
+}
 
+output "token_url" {
+  value       = format("https://%s.auth.%s.amazoncognito.com/oauth2/token", aws_cognito_user_pool.pool.domain, data.aws_region.current.name)
+  description = "The URL for receiving an access token using the OAuth 2.0 client credentials flow."
+}
+
+output "client_ids" {
+  value = tomap({
+    for k, client in aws_cognito_user_pool_client.client : k => client.id
+  })
+  description = "The client IDs."
+}
+
+output "client_secrets" {
+  value = tomap({
+    for k, client in aws_cognito_user_pool_client.client : k => client.client_secret
+  })
+  description = "The client secrets."
+}
--- variables.tf
+++ variables.tf
@@ -3,6 +3,27 @@ variable "resource_prefix" {
   description = "The text every created resource will be prefixed with."
 }
 
+variable "resource_server_identifier" {
+  type        = string
+  default     = "tilores"
+  description = "The name of the resource server that will be created."
+}
+
+variable "available_scopes" {
+  type = list(object({
+    name        = string
+    description = string
+  }))
+  description = "The available scopes that can be assigned to a client. Note, that each scope when used will be prefixed with the resource_server_identifier."
+}
+
+variable "clients" {
+  type = map(object({
+    allowed_scopes = list(string)
+  }))
+  description = "The clients to create."
+}
+
 locals {
   prefix = format("%s-tilores", var.resource_prefix)
 }
