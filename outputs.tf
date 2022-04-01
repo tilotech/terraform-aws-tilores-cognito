deb2ebddfa5b895e9e1612ac915bd8e82f49fb4e
Stefan Berkner <stefan.berkner@tilotech.io>
2022-04-01 17:11:41 +0200

    Add further outputs

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "token_url" {
   description = "The URL for receiving an access token using the OAuth 2.0 client credentials flow."
 }
 
+output "user_pool_domain" {
+  value       = format("%s.auth.%s.amazoncognito.com", aws_cognito_user_pool_domain.domain.domain, data.aws_region.current.name)
+  description = "The domain name of the user pool, used by all OAuth 2.0 flows."
+}
+
 output "client_ids" {
   value = tomap({
     for k, client in aws_cognito_user_pool_client.client : k => client.id
@@ -20,4 +25,14 @@ output "client_secrets" {
     for k, client in aws_cognito_user_pool_client.client : k => client.client_secret
   })
   description = "The client secrets."
+}
+
+output "cloudfront_distribution_arn" {
+  value       = aws_cognito_user_pool_domain.domain.cloudfront_distribution_arn
+  description = "The URL of the CloudFront distribution. This is required to generate the ALIAS aws_route53_record."
+}
+
+output "user_pool_id" {
+  value       = aws_cognito_user_pool.pool.id
+  description = "The ID of the created Cognito user pool."
 }
